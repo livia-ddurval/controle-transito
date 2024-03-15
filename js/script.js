bc4291488df5163b84922dea334f477173e46ac7
LÍVIA MARIA DURVAL BESERRA <liviao_28@outlook.com>
2024-03-15 15:16:47 -0300

    segundo commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// function launchRocket() {
+//     const rocket = document.getElementById("rocket");
+//     const rocketActive = document.getElementById("rocket-active");
+  
+//     rocketActive.classList.add("launch-animation");
+//     rocket.style.display = "none";
+//     rocketActive.style.display = "flex";
+  
+//     setTimeout(() => {
+//       rocketActive.classList.remove("launch-animation");
+//       rocket.style.display = "flex";
+//       rocketActive.style.display = "none";
+//     }, 2000);
+//   }
